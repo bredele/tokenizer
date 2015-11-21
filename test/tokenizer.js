61a6542752c11fd103326f51e334cba63bcf65c6
bredele <olivier.wietrich@gmail.com>
2015-11-20 17:45:23 -0700

    add special characters test

--- test/tokenizer.js
+++ test/tokenizer.js
@@ -12,5 +12,14 @@ describe('basic', function() {
 	it('split words', function() {
 		assert.deepEqual(tokenizer('it is beauty'), ['it', 'is', 'beauty']);
 	});
+
+});
+
+
+describe('special characters', function() {
+
+	it('split words with dots', function() {
+		assert.deepEqual(tokenizer('it is beauty.'), ['it', 'is', 'beauty']);
+	});
 	
 });
